190427f0bb32ac9b8fb0c4f01357ea9b3afbbe09
Tos445 <sebastian.lukasz.pawlikowski@gmail.com>
2016-08-15 21:25:32 +0200

    Initial commit
    Initial commit with all changes

---
+++ AsistentoDelProyecto/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
+using System.ComponentModel;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace AsistentoDelProyecto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InicializeWindow();
+
+            taskbarIcon.TrayMouseDoubleClick += new RoutedEventHandler(OnTrayMouseDoubleClick);            
+        }
+                
+        private void InicializeWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == System.Windows.WindowState.Minimized)
+            {
+                this.Hide();
+                taskbarIcon.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                taskbarIcon.Visibility = Visibility.Hidden;
+            }
+            
+            base.OnStateChanged(e);
+        }
+
+        private void OnTrayMouseDoubleClick(object sender, RoutedEventArgs args)
+        {
+            this.Show();
+            this.WindowState = WindowState.Normal;
+        }
+    }
+}
