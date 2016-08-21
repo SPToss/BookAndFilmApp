364c9b7aee20e1089a739f297c25bf2e8d8cfdba
Tos445 <sebastian.lukasz.pawlikowski@gmail.com>
2016-08-21 20:37:29 +0200

    XML Serialize

---
+++ DataAcces/XmlHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace DataAcces
+{
+    public static class XmlHelper
+    {
+        public static T Deserialize<T> (XElement element)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(element.CreateReader());
+        }
+    }
+}
