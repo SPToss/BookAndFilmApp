92b6c37da8fa35198c388cc124272da2d89be456
Tos445 <sebastian.lukasz.pawlikowski@gmail.com>
2016-11-03 22:29:08 +0100

    Some Unit Tests

---
+++ UnitTest/DomainTest/FilmServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+
+namespace UnitTest.DomainTest
+{
+    public class FilmServiceTest : FilmServiceTestBase
+    {
+        [Test]
+        public void AfterAddTwoFilmServiceShouldHaveTwoElement()
+        {
+            Actual = filmService.GetAllItemsCount();
+            Expected = 2;
+        }
+
+        [Test]
+        public void ServiceShouldHaveCategory()
+        {
+            Actual = filmService.CHeckForSerie("Test name");
+            Expected = true;
+        }
+    }
+}
---
+++ UnitTest/DomainTest/FilmServiceTestBase.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using Domain.Objects;
+using Domain.Services;
+using System.Collections.Generic;
+
+namespace UnitTest.DomainTest
+{
+    public abstract class FilmServiceTestBase
+    {
+        protected FilmService filmService;
+        protected object Expected;
+        protected object Actual;
+        [SetUp]
+        public void BeforeEach()
+        {
+            var films = new Films
+            {
+                FilmSeries = new List<FilmSerie>
+                {
+                    new FilmSerie
+                    {
+                        Category = "Test category",
+                        Name = "Test name",
+                        Films = new List<Film>
+                        {
+                            new Film
+                            {
+                                Distribution = "Test Distribution 1",
+                                FilmStatus = DataTransfer.FilmStatus.Canceled,
+                                Genres = "Test Geners 1",
+                                ProductionCompany = "Test Company 1",
+                                ReliseYear = "2014/05/05",
+                                SoundTrack = DataTransfer.FilmSoundTrack.Dubbing,
+                                Title = "Test Title 1",
+                                Type = "Test Type 1"
+                            },
+                            new Film
+                            {
+                                Distribution = "Test Distribution 2",
+                                FilmStatus = DataTransfer.FilmStatus.Canceled,
+                                Genres = "Test Geners 2",
+                                ProductionCompany = "Test Company 2",
+                                ReliseYear = "2014/05/05",
+                                SoundTrack = DataTransfer.FilmSoundTrack.Dubbing,
+                                Title = "Test Title 2",
+                                Type = "Test Type 2"
+                            }
+                        }
+                    }
+                }
+            };
+            filmService = new FilmService(films,"");
+        }
+        [TearDown]
+        public void AfterAll()
+        {
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
